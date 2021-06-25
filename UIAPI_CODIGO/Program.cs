5441cf332afe54db62ed27d6a52e6ac7fa321f3b
benjamin <riders_23x@hotmail.com>
2021-06-25 10:56:04 -0500

    cambio

--- UIAPI_CODIGO/Program.cs
+++ UIAPI_CODIGO/Program.cs
@@ -21,6 +21,7 @@ namespace UIAPI_CODIGO
             //agregarmenus();
             // benjamin
             /// cambio hoy dia
+            /// otro cambio
             Menus();
             SBO_Application.ItemEvent += new SAPbouiCOM._IApplicationEvents_ItemEventEventHandler(SBO_Application_ItemEvent);
             SBO_Application.MenuEvent += new SAPbouiCOM._IApplicationEvents_MenuEventEventHandler(SBO_Application_MenuEvent);
